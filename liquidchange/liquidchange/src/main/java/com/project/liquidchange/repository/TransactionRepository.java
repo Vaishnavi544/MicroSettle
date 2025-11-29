e0a11b0251b294807eeafa583f3357801b0881f2
Vaishnavi544 <142041825+Vaishnavi544@users.noreply.github.com>
2025-11-29 22:57:30 +0530

    Delete liquidchange/liquidchange directory

--- liquidchange/liquidchange/src/main/java/com/project/liquidchange/repository/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.liquidchange.repository;
-
-import com.project.liquidchange.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    // We don't need custom methods yet, the standard ones are enough.
-}
