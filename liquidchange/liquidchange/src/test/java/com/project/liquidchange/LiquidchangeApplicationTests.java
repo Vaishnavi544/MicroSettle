5d00295fc0806f7230ed8e3c7ea82b3474629a8c
Vaishnavi544 <142041825+Vaishnavi544@users.noreply.github.com>
2025-11-29 22:05:42 +0530

    Add files via upload

---
+++ liquidchange/liquidchange/src/test/java/com/project/liquidchange/LiquidchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.liquidchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
